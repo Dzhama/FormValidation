4c8ad89d6fa9934743398855fa4318c11ef8110c
Dzhama <j.shoismatuloev@gmail.com>
2018-05-21 15:15:40 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+// From Blur Event listener
+document.getElementById('name').addEventListener('blur', validateName);
+document.getElementById('zip').addEventListener('blur', validateZip);
+document.getElementById('email').addEventListener('blur', validateEmail);
+document.getElementById('phone').addEventListener('blur', validatePhone);
+
+
+// function 
+
+function validateName() {
+
+};
+
+function validateZip() {
+
+};
+
+function validateEmail() {
+
+};
+
+function validatePhone() {
+
+}
